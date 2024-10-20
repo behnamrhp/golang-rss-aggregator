2c6f24844bb74a7e08fcf6cc2eab49941a93b4c0
behnamrhp <behnamrahimpour74@gmail.com>
2024-10-21 00:17:41 +0300

    [FEAT]: Add web server with chi

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/behnamrhp/golang-rss-aggregator.git
 
 go 1.23.1
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	port := getPort()
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	fmt.Printf("Server is runnin on the port: %v", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+}
+
+func getPort() string {
 	godotenv.Load()
 
 	port := os.Getenv("PORT")
@@ -16,6 +35,5 @@ func main() {
 	if port == "" {
 		log.Fatal("Port is not set")
 	}
-
-	fmt.Println("port is", port)
+	return port
 }
